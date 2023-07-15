0195b9f37d1800ddc4d2c697460c476aead8a8f1
Vikram Kangotra <vikramkangotra8055@gmail.com>
2023-07-16 02:13:54 +0530

    refactored

--- src/main.cpp
+++ src/main.cpp
@@ -12,33 +12,8 @@
 #include "window/window.h"
 #include "renderer/renderer.h"
 #include "event/event.h"
+#include "epicycloid.h"
 
-class Epicycloid {
-    public:
-        Epicycloid(const int radius, const float frequency, const int size)
-        : radius{radius}, angle{}, frequency{frequency}, size{size} {}
-
-        void draw(Renderer& renderer) {
-            renderer.drawCircle(x, y, size);
-        }
-
-        void update() {
-            angle += frequency;
-        }
-
-        void cycleAround(const int centerX, const int centerY) {
-            x = radius * sin(angle) + centerX;
-            y = radius * cos(angle) + centerY;
-        }
-
-    public:
-        int radius;
-        float angle;
-        float frequency;
-        int x;
-        int y;
-        int size;
-};
 
 int main() {
 
